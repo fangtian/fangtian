fdc8e34c43c667185c842648362cc6d7a844aafd
tslxt <15460854@qq.com>
2016-08-21 02:13:01 +0800

    add chapter list in mycourse.php

---
+++ _/components/php/chapter_list.php
@@ -0,0 +1,90 @@
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th>讲次</th>
+      <th>主题</th>
+      <th>上课时间</th>
+      <th>天天练成绩</th>
+      <th>课前小测成绩</th>
+      <th>额外联系成绩</th>
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      <td colspan="6">
+        <div class="bulk-actions align-left"></div>
+        <div class="pagination"> </div>
+        <!-- End .pagination -->
+        <div class="clear"></div>
+      </td>
+    </tr>
+  </tfoot>
+  <tbody>
+    <tr class="alt-row">
+        <td>1</td>
+        <td>六年级暑假第一讲</td>
+        <td>2016-07-09 09:00</td>
+        <td>82</td>
+        <td>100</td>
+        <td>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+        </td>
+      </tr><tr>
+        <td>2</td>
+        <td>六年级暑假第二讲</td>
+        <td>2016-07-09 09:00</td>
+        <td class="bg-danger">未提交</td>
+        <td>100</td>
+        <td>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+        </td>
+      </tr><tr class="alt-row">
+        <td>3</td>
+        <td>六年级暑假第三讲</td>
+        <td>2016-07-09 09:00</td>
+        <td class="bg-warning">未批复</td>
+        <td>100</td>
+        <td>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+        </td>
+      </tr><tr>
+        <td>4</td>
+        <td>六年级暑假第四讲</td>
+        <td>2016-07-09 09:00</td>
+        <td>82</td>
+        <td>100</td>
+        <td>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+        </td>
+      </tr><tr class="alt-row">
+        <td>5</td>
+        <td>六年级暑假第五讲</td>
+        <td>2016-07-09 09:00</td>
+        <td>82</td>
+        <td>100</td>
+        <td>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+        </td>
+      </tr>       
+    </tbody>
+</table>
